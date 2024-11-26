7a0ec67ab65a9a7dd85cf0f0b4d4838718011e80
Kyle J. McKeown <mckeown_k@hotmail.com>
2024-11-26 13:59:32 -0500

    Sync park pattern when turning lights on while already parked

--- ulc/client/c_park.lua
+++ ulc/client/c_park.lua
@@ -8,7 +8,7 @@ local effectDelay = 1000
 CreateThread(function()
     while true do
         if IsPedInAnyVehicle(PlayerPedId()) then
-            TriggerEvent('ulc:checkParkState', veh, false)
+            TriggerEvent('ulc:checkParkState', false)
 
             Wait(Config.ParkSettings.delay * 1000)
         else
@@ -41,7 +41,7 @@ RegisterNetEvent("ulc:checkParkState", function(delay)
 end)
 
 AddEventHandler('ulc:vehPark', function()
-    if Lights then
+    --if Lights then
         --print('[ulc:vehPark] My vehicle is parked.')
         parked = true
 
@@ -55,72 +55,9 @@ AddEventHandler('ulc:vehPark', function()
                 ULC:SetStage(v, 1, false, true, false, false, true, false)
             end
 
-            -- park pattern sync stuff
-            if MyVehicleConfig.parkConfig.useSync then
-                -- cooldown
-                local gameSeconds = GetGameTimer() / 1000
-                if gameSeconds >= lastSync + Config.ParkSettings.syncCooldown then
-                    lastSync = gameSeconds
-
-                    local loadedVehicles = GetGamePool("CVehicle")
-                    --print(#loadedVehicles .. " vehicles in pool")
-                    local vehsToSync = {}
-
-                    for k, v in pairs(loadedVehicles) do
-                        -- don't include my vehicle
-                        if v ~= veh then
-                            local vehCoords = GetEntityCoords(v)
-                            local pedCoords = GetEntityCoords(PlayerPedId())
-                            local distance = GetDistanceBetweenCoords(vehCoords, pedCoords)
-
-
-                            if distance < Config.ParkSettings.syncDistance then
-                                if GetVehicleClass(v) == 18 then
-                                    -- check if my vehicle is set to sync with this vehicle or if the vehicle is the same model as my vehicle
-                                    if IsVehicleInTable(v, MyVehicleConfig.parkConfig.syncWith) or GetEntityModel(v) == GetEntityModel(MyVehicle) then
-                                        --print('Vehicle' .. v .. ' should sync with me.')
-
-                                        local speed = GetVehicleSpeedConverted(veh)
-
-                                        if speed < Config.ParkSettings.speedThreshold then
-                                            --print("Found an eligible sync vehicle.")
-                                            table.insert(vehsToSync, v)
-                                        end
-                                    end
-                                end
-                            end
-                        end
-                    end
-                    if #vehsToSync > 0 then
-                        -- sync my vehicle
-                        SetVehicleSiren(veh, false)
-                        SetVehicleSiren(veh, true)
-
-                        -- sync other vehicles on my screen
-                        for k, v in pairs(vehsToSync) do
-                            if IsVehicleSirenOn(v) then
-                                SetVehicleSiren(v, false)
-                                SetVehicleSiren(v, true)
-                            end
-                        end
-
-                        -- send sync to other clients nearby
-                        --print("Preparing to send sync to server")
-                        local vehsToSyncNet = {}
-                        for k, v in pairs(vehsToSync) do
-                            --print("Candidate: " .. VehToNet(v))
-                            table.insert(vehsToSyncNet, VehToNet(v))
-                        end
-                        TriggerServerEvent("sync:send", vehsToSyncNet)
-                    else --print('Found no vehicles to sync.')
-                    end
-                else
-                    print("Sync on cooldown, time left: " ..
-                        Config.ParkSettings.syncCooldown - (gameSeconds - lastSync) .. " seconds.")
-                end
-            end
+            TriggerEvent('ulc:vehSync')
         end
-    end
+    --end
 end)
 
 RegisterNetEvent('ulc:sync:receive', function(vehicles)
@@ -133,7 +70,7 @@ RegisterNetEvent('ulc:sync:receive', function(vehicles)
 end)
 
 AddEventHandler('ulc:vehDrive', function()
-    if Lights then
+    --if Lights then
         --print('[ulc:vehDrive] My vehicle is driving.')
         parked = false
         if MyVehicle and MyVehicleConfig.parkConfig.usePark then
@@ -146,5 +83,87 @@ AddEventHandler('ulc:vehDrive', function()
                 ULC:SetStage(v, 0, false, true, false, false, true, false)
             end
         end
+    --end
+end)
+
+AddEventHandler('ulc:vehSync', function()
+    -- park pattern sync stuff
+    if MyVehicleConfig.parkConfig.useSync then
+        -- cooldown
+        local gameSeconds = GetGameTimer() / 1000
+        if gameSeconds >= lastSync + Config.ParkSettings.syncCooldown then
+            lastSync = gameSeconds
+
+            local loadedVehicles = GetGamePool("CVehicle")
+            --print(#loadedVehicles .. " vehicles in pool")
+            local vehsToSync = {}
+
+            for k, v in pairs(loadedVehicles) do
+                -- don't include my vehicle
+                if v ~= veh then
+                    local vehCoords = GetEntityCoords(v)
+                    local pedCoords = GetEntityCoords(PlayerPedId())
+                    local distance = GetDistanceBetweenCoords(vehCoords, pedCoords)
+
+
+                    if distance < Config.ParkSettings.syncDistance then
+                        if GetVehicleClass(v) == 18 then
+                            -- check if my vehicle is set to sync with this vehicle or if the vehicle is the same model as my vehicle
+                            if IsVehicleInTable(v, MyVehicleConfig.parkConfig.syncWith) or GetEntityModel(v) == GetEntityModel(MyVehicle) then
+                                --print('Vehicle' .. v .. ' should sync with me.')
+
+                                local speed = GetVehicleSpeedConverted(veh)
+
+                                if speed < Config.ParkSettings.speedThreshold then
+                                    --print("Found an eligible sync vehicle.")
+                                    table.insert(vehsToSync, v)
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+            if #vehsToSync > 0 then
+                -- sync my vehicle
+                SetVehicleSiren(veh, false)
+                SetVehicleSiren(veh, true)
+
+                -- sync other vehicles on my screen
+                for k, v in pairs(vehsToSync) do
+                    if IsVehicleSirenOn(v) then
+                        SetVehicleSiren(v, false)
+                        SetVehicleSiren(v, true)
+                    end
+                end
+
+                -- send sync to other clients nearby
+                --print("Preparing to send sync to server")
+                local vehsToSyncNet = {}
+                for k, v in pairs(vehsToSync) do
+                    --print("Candidate: " .. VehToNet(v))
+                    table.insert(vehsToSyncNet, VehToNet(v))
+                end
+                TriggerServerEvent("sync:send", vehsToSyncNet)
+            else --print('Found no vehicles to sync.')
+            end
+        else
+            print("Sync on cooldown, time left: " ..
+                Config.ParkSettings.syncCooldown - (gameSeconds - lastSync) .. " seconds.")
+        end
+    end
+end)
+
+CreateThread(function()
+    local prevLights = false
+
+    while true do
+        if Lights ~= prevLights then
+            prevLights = Lights
+
+            if Lights then
+                TriggerEvent('ulc:vehSync')
+            end
+        end
+        Wait(0)
     end
 end)
